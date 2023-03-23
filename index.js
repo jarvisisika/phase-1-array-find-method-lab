9466fc4678d92f076c3c2c4d579f013949eaf01f
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-03-24 00:48:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(array) {
+    const winningYear = array.find(object => object.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
+  
